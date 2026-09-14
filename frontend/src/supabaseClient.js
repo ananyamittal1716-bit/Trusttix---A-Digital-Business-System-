5726cdd9800f5a7735f762bd4329de94910f3774
divyanshagarwal2301-lab <divyanshagarwal2301@gmail.com>
2026-09-14 19:36:49 +0530

    Delete frontend directory

--- frontend/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://gszgvadlbmagtbhsyqbn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdzemd2YWRsYm1hZ3RiaHN5cWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNDc1MDgsImV4cCI6MjEwNDYyMzUwOH0.cVeE-mpB3A2IaMOFiKZGzAUdB6cEnE1qOFoBNJl46BI';
-
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
